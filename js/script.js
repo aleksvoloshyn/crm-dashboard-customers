7dda2909e14ab37bc4d7c3b6b6340a0dfe2ec012
Alexei Voloshyn <75684845+aleksvoloshyn@users.noreply.github.com>
2023-04-03 22:14:22 +0300

    board and pagination added

--- js/script.js
+++ js/script.js
@@ -1,4 +1,8 @@
 const pageNavItem = document.querySelectorAll('.page-navigation__item')
+const mainTitle = document.querySelector('.page-board__main-title')
+const activeTitle = document.querySelector('.page-board__active-title')
+const inActive = document.querySelectorAll('.page-board__tablecell--inactive')
+const pageBoardItem = document.querySelectorAll('.page-board__item')
 
 function menuItemsHandler() {
   const activeLink = document.querySelector('.isActive')
@@ -10,6 +14,30 @@ function menuItemsHandler() {
   curActiveText.classList.add('isActiveText')
 }
 
+function activeCustomersHandler() {
+  inActive.forEach((i) => i.parentNode.classList.add('isHidden'))
+}
+function allCustomersHandler() {
+  inActive.forEach((i) => i.parentNode.classList.remove('isHidden'))
+}
+
 pageNavItem.forEach((item) => {
   item.addEventListener('click', menuItemsHandler)
+})
+
+activeTitle.addEventListener('click', activeCustomersHandler)
+mainTitle.addEventListener('click', allCustomersHandler)
+
+pageBoardItem.forEach((item) => {
+  item.addEventListener('click', function () {
+    const activePage = document.querySelector('.active-page')
+
+    if (
+      this.innerText !== '<' &&
+      (this.innerText !== '...') & (this.innerText !== '>')
+    ) {
+      activePage.classList.remove('active-page')
+      this.classList.add('active-page')
+    }
+  })
 })
